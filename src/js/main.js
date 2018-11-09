4bde00e797dcf46415c60bf0893507f1c387aec2
Joaovsa7 <41493938+Joaovsa7@users.noreply.github.com>
2018-11-09 14:30:54 -0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,20 @@
 let bandeiras = document.querySelectorAll('#bandeira')
 let legendaFlag = document.querySelector('#texto-flag')
 let menuSlider = document.querySelector('#menuCidades')
-        menuSlider.style.display = 'none'
-function MudarCor(){
+menuSlider.style.display = "none"
+
+bandeira[0].onclick = () => {
     legendaFlag.style.display = 'none'
     bandeiras[1].classList.add('grayscale')
     bandeiras[2].classList.add('grayscale')
     bandeiras[3].classList.add('grayscale')
     bandeiras[4].classList.add('grayscale')
-    if (menuSlider.style.display === "none"){
-        menuSlider.style.display = "block";      
-    }   
-    else { 
-        menuSlider.style.display = 'none'
-        legendaFlag.style.display = 'block'
+    if(menuSlider.style.display === "none"){
+        menuSlider.style.display = "block"
+    }
+    else{ 
+        legendaFlag.style.display = "block"
+        menuSlider.style.display = "none"
         bandeiras[1].classList.replace('grayscale', 'cor')
         bandeiras[2].classList.replace('grayscale', 'cor')
         bandeiras[3].classList.replace('grayscale', 'cor')
