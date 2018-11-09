aee73c2a17304c8ed52765c1e255a2dba3790007
joao <Gustavo@iMac-de-5ERA-4.local>
2018-11-09 11:05:10 -0200

    ajustes-animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,24 @@
 let bandeiras = document.querySelectorAll('#bandeira')
 let legendaFlag = document.querySelector('#texto-flag')
 let menuSlider = document.querySelector('#menuCidades')
+let containerClique = document.querySelector('.main-container')
 menuSlider.style.display = 'none'
+
 function MudarCor(){
-    bandeiras[0].onclick  = () => {
-        legendaFlag.style.display = 'none'
-        bandeiras[1].classList.add('grayscale')
-        bandeiras[2].classList.add('grayscale')
-        bandeiras[3].classList.add('grayscale')
-        bandeiras[4].classList.add('grayscale')
-        if (menuSlider.style.display === "none"){
-            menuSlider.style.display = "block";      
-        }   
-        
-        else {
-            menuSlider.style.display = "none";      
-            legendaFlag.style.display = 'block'
-            clique()
-            bandeiras[1].classList.replace('grayscale', 'cor')
-            bandeiras[2].classList.replace('grayscale', 'cor')
-            bandeiras[3].classList.replace('grayscale', 'cor')
-            bandeiras[4].classList.replace('grayscale', 'cor')
-        }
+    legendaFlag.style.display = 'none'
+    bandeiras[1].classList.add('grayscale')
+    bandeiras[2].classList.add('grayscale')
+    bandeiras[3].classList.add('grayscale')
+    bandeiras[4].classList.add('grayscale')
+    if (menuSlider.style.display === "none"){
+        menuSlider.style.display = "block";      
+    }   
+    else { 
+        menuSlider.style.display = 'none'
+        legendaFlag.style.display = 'block'
+        bandeiras[1].classList.replace('grayscale', 'cor')
+        bandeiras[2].classList.replace('grayscale', 'cor')
+        bandeiras[3].classList.replace('grayscale', 'cor')
+        bandeiras[4].classList.replace('grayscale', 'cor')
     }
 }
-
-MudarCor()
